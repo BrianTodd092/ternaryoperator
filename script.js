a5e120371eacb24076a120a0a9e3a8fab075d212
Brian Todd <44840863+BrianTodd092@users.noreply.github.com>
2023-12-10 05:42:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ tInterval = setInterval(setColor, 1000);
 
 
 function setColor(){
-let x = document.body;                                // whitespace if,  if else.
+let x = document.body;                                // whitespace if,  else.
 x.style.backgroundColor = x.style.backgroundColor == "yellow" ? "green" : "yellow";
 }
